bada55a49550956cd180528cd12a93cb7fb14e55
CyanAutomation <68898852+CyanAutomation@users.noreply.github.com>
2026-05-03 17:17:11 +0000

    refactor: Update Jest configuration for ESM support and module mapping

--- jest.config.ts
+++ jest.config.ts
@@ -12,12 +12,19 @@ const config: Config = {
   ],
   coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  extensionsToTreatAsEsm: ['.ts'],
   transform: {
     '^.+\\.ts$': ['ts-jest', {
+      useESM: true,
       tsconfig: {
         esModuleInterop: true,
+        module: 'ESNext',
       },
     }],
+  },
+  transformIgnorePatterns: ['node_modules/(?!(uuid)/)'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
   },
 };
 
