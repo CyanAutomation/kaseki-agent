cd0355b1b92d8b19ab3f47608466eab1d64407ea
CyanAutomation <68898852+CyanAutomation@users.noreply.github.com>
2026-05-25 16:54:42 +0100

    test: strengthen SetupContext type assertions

--- src/kaseki-api/setup-context.type-test.ts
+++ src/kaseki-api/setup-context.type-test.ts
@@ -1,4 +1,5 @@
 import { initializeSetup, type SetupContext } from './setup-orchestrator';
+import type { SetupContext as BarrelSetupContext } from './index';
 
 type Assert<T extends true> = T;
 type IsExact<T, U> = (<G>() => G extends T ? 1 : 2) extends <G>() =>
@@ -21,5 +22,22 @@ type _setupContextHasExpectedProperties = Assert<
   >
 >;
 
+type _barrelExportsSetupContextAlias = Assert<
+  IsExact<BarrelSetupContext, SetupContext>
+>;
+
+type _setupContextShape = Assert<
+  IsExact<
+    SetupContext,
+    {
+      nodeVersionValid: boolean;
+      templateInitialized: boolean;
+      templateDir: string;
+    }
+  >
+>;
+
 void (0 as unknown as _initializeSetupReturnsSetupContext);
 void (0 as unknown as _setupContextHasExpectedProperties);
+void (0 as unknown as _barrelExportsSetupContextAlias);
+void (0 as unknown as _setupContextShape);
